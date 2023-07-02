e2bbb7075cf4896bfca4112ec29ac4635de581a2
Stijn Settleman <32292824+stijnklomp@users.noreply.github.com>
2023-07-02 18:04:23 +0100

    Adding 'ts-jest' for ESM unit testing (#2)

---
+++ test/unit/main.test.tsx
@@ -0,0 +1,9 @@
+import { sum } from "../../controllers/sum"
+// import { sum } from "controllers/sum"
+
+describe("sum module", () => {
+	test("adds 1 + 2 to equal 3", () => {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+		expect(sum(1, 2)).toBe(3)
+	})
+})
