7f5c25370f5675329274dd3b2cf1288def85752a
Stijn Settleman <32292824+stijnklomp@users.noreply.github.com>
2023-07-14 21:43:54 +0100

    Clean up code (#8)
    * Add coverage reports to gitignore && Change config files to JSON format
    
    * Update dependencies && Fix for Next config

--- tests/unit/main.test.tsx
+++
@@ -1,9 +0,0 @@
-import { sum } from "@/features/numberCalculations/sum"
-// import { sum } from "../../features/numberCalculations/sum"
-
-describe("sum module", () => {
-	test("adds 1 + 2 to equal 3", () => {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-		expect(sum(1, 2)).toBe(3)
-	})
-})
