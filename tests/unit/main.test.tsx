7824740ad26d8c2aff31c9144ed30e2cab06479a
stijnklomp <stijnklomp1@hotmail.com>
2023-08-02 23:12:18 +0100

    Update linting and formatting rules

---
+++ tests/unit/main.test.tsx
@@ -0,0 +1,8 @@
+// import { sum } from "@/features/numberCalculations/sum"
+
+// describe("sum module", () => {
+// 	test("adds 1 + 2 to equal 3", () => {
+// 		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+// 		expect(sum(1, 2)).toBe(3)
+// 	})
+// })
