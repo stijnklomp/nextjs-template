e78dbaee79212bb34ceda782d934f5d70549fedc
Stijn Settleman <32292824+stijnklomp@users.noreply.github.com>
2023-08-02 23:17:40 +0100

    Update linting and formatting rules (#10)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,24 @@
 module.exports = {
 	root: true,
 	extends: [
-		"./node_modules/stijnklomp-eslint-config/eslint.js",
+		"./node_modules/stijnklomp-eslint-config/eslintRules.js",
+		"./node_modules/stijnklomp-eslint-config/jestRules.js",
+		"./node_modules/stijnklomp-eslint-config/typescriptRules.js",
 		"next/core-web-vitals"
 	],
 	parser: "@typescript-eslint/parser",
 	plugins: ["@typescript-eslint", "prettier"],
 	parserOptions: {
 		ecmaVersion: 2021,
-		srouceType: "module"
+		sourceType: "module"
 	},
 	env: {
 		browser: true,
 		node: true,
 		commonjs: true,
 		es6: true
+	},
+	settings: {
+		"prettier/prettier": require("./node_modules/stijnklomp-eslint-config/prettierRules.js")
 	}
 }
