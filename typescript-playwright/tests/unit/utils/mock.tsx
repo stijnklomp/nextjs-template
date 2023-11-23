8dc2a60af05f15f11010a4d9645b075dd091d272
stijnklomp <stijnklomp1@hotmail.com>
2023-11-23 20:55:31 +0000

    Playwright configuration

--- typescript-playwright/tests/unit/utils/mock.tsx
+++ typescript-playwright/tests/unit/utils/mock.tsx
@@ -3,7 +3,7 @@
  * even if they are asynchronous and haven't happened yet
  */
 export const mockAllTimeout = () => {
-	;(global.setTimeout as unknown as jest.Mock).mockImplementation(
+	(global.setTimeout as unknown as jest.Mock).mockImplementation(
 		(cb: () => void) => {
 			cb()
 
