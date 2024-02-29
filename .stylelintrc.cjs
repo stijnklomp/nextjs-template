6f9ab08ce1bc926b8b3003f1de0a71646ef8e8c1
stijnklomp <stijnklomp1@hotmail.com>
2024-02-29 00:15:43 +0000

    Updated configuration

---
+++ .stylelintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+	extends: [
+		"./node_modules/stijnklomp-linting-formatting-config/stylelint/stylelint.js",
+		"./node_modules/stijnklomp-linting-formatting-config/stylelint/stylelintRules.js"
+	]
+}
