4d0957334f89b5c2a72ec740516ee114bfa550e7
Stijn Klomp <stijnklomp1@hotmail.com>
2026-06-13 21:24:55 +0100

    Allow anyone to connect on local network in dev mode

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin()
 /** @type {import("next").NextConfig} */
 const nextConfig = {
 	output: "standalone",
+	allowedDevOrigins: ["*.*.*.*"],
 	experimental: {
 		optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
 	},
